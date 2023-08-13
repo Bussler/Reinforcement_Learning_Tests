60aff657f409cdd31b2f7e6384a6b96208a9bd23
Bussler <maarten.bussler@gmail.com>
2023-08-13 13:02:41 +0200

    Add local storage of llm and chromadb form LLM_QA

--- Huggingfacetest/LLM_QA.py
+++ Huggingfacetest/LLM_QA.py
@@ -1,4 +1,5 @@
 from langchain import HuggingFaceHub
+import huggingface_hub
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.document_loaders import DirectoryLoader
@@ -9,14 +10,30 @@ from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 
+from langchain.llms import HuggingFacePipeline
+from langchain.embeddings import SentenceTransformerEmbeddings
+from langchain.chains import ConversationalRetrievalChain
+
 
 hugging_face_token = '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'
 
 
 # M: Hugging face setup
-llm = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature": 0, "max_length":64},
-                     huggingfacehub_api_token=hugging_face_token)  # can use gpt2 also as repo_id, google/flan-t5-xl, flan-t5-small /base
+#llm = HuggingFaceHub(repo_id="google/flan-t5-small", model_kwargs={"temperature": 0, "max_length":64},
+#                     huggingfacehub_api_token=hugging_face_token)  # can use gpt2 also as repo_id, google/flan-t5-xl, flan-t5-small, base
 
+# M: download and run llm locally
+#huggingface_hub.login(token=hugging_face_token)
+llm = HuggingFacePipeline.from_model_id(
+        model_id="bigscience/bloom-560m", # Small model; stupid but works for testing
+        # model_id="meta-llama/Llama-2-7b-chat-hf", # Big model; only works on TERA
+        # model_id="google/flan-t5-small", # Small model, is not that good. Also need to change task to "text-generation", if we use this!
+        task="text-generation",
+        #task="text2text-generation",
+        model_kwargs={"temperature": 0, "max_length": 300},#, "use_auth_token": True},
+        device=-1, # CPU
+        # device=0, # GPU
+    )
 
 #print(llm("translate to German: How are you"))
 
@@ -25,7 +42,6 @@ llm = HuggingFaceHub(repo_id="google/flan-t5-base", model_kwargs={"temperature":
 #question = "Who started ecomomic liberliation in India and what was the imnpact of it on India economically?"
 #llm_chain=LLMChain(prompt=prompt,llm=llm)
 #print(llm_chain.run(question))
-
 
 # M: document loaders: parse in the docs
 print("parse in text documents")
@@ -39,9 +55,9 @@ docs = text_splitter.split_documents(documents=documents)
 #docs = text_splitter.create_documents(docs) # M: use this for string input, e.g. from file.read()
 
 # M: Embedder for the vector store to transform the text into vector space
-# TODO: try Sentence_Transformers for this too!
 print("embeddings")
-embeddings = HuggingFaceEmbeddings()
+embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+#embeddings = SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2") # M: equivalent!
 
 #query = "send me full list of conacts from contact list"
 #embedding_vector = embeddings.embed_query(query)
@@ -50,13 +66,24 @@ embeddings = HuggingFaceEmbeddings()
 
 # create a vector store for these texts using the 'embeddings'
 print("vector store")
-doc_search = Chroma.from_documents(docs, embeddings)
+doc_search = Chroma.from_documents(docs, embeddings, persist_directory='chroma_db')
 
 # M: Using langchain chains to build qna: Incoming message is embedded, passed to the llm, which uses the vectorspace to find closely related words to the message
-qna = RetrievalQA.from_chain_type(llm= llm, chain_type="stuff", retriever=doc_search.as_retriever())
+#qna = RetrievalQA.from_chain_type(llm= llm, chain_type="stuff", retriever=doc_search.as_retriever())
+#query = "send me full list of conacts from contact list"
+#print("Query: ", query)
+#print(qna.run(query))
 
+# M: build chat history to take into account
+qna = ConversationalRetrievalChain.from_llm(
+        llm, 
+        doc_search.as_retriever(), 
+        return_source_documents=True
+    )
 
 query = "send me full list of conacts from contact list"
-print(f"Query: {query}")
-print("Answer:\n", qna.run(query))
+chat_history = []
+print("Query: ", query)
+result = qna({"question": query, "chat_history": chat_history})
+print(result['answer'])
 pass
