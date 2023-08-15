ce7cc4de896bf01be117014eaee7b8570f24c2fe
Bussler <maarten.bussler@gmail.com>
2023-08-15 17:32:50 +0200

    langchain pipeline gives weird results when the parameter repetition_penalty is set

--- Huggingfacetest/LLM_QA.py
+++ Huggingfacetest/LLM_QA.py
@@ -16,6 +16,8 @@ from langchain.chains import ConversationalRetrievalChain
 import transformers
 import torch
 from langchain.vectorstores import FAISS
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 device = f'cuda:{torch.cuda.current_device()}' if torch.cuda.is_available() else 'cpu'
 hugging_face_token = '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'
@@ -27,78 +29,85 @@ hugging_face_token = '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'
 
 #2) M: download and run llm locally with langchain wrapper. Watch out: is deprecated
 #huggingface_hub.login(token=hugging_face_token)
-llm = HuggingFacePipeline.from_model_id(
-        model_id="bigscience/bloom-560m", # Small model; stupid but works for testing
-        # model_id="meta-llama/Llama-2-7b-chat-hf", # Big model; only works on TERA
-        # model_id="google/flan-t5-small", # Small model, is not that good. Also need to change task to "text-generation", if we use this!
-        task="text-generation",
-        #task="text2text-generation",
-        model_kwargs={"temperature": 0, "max_length": 300},#, "use_auth_token": True},
-        #device=-1, # CPU
-        device=0, # GPU
-    )
+#llm = HuggingFacePipeline.from_model_id(
+#        #model_id="bigscience/bloom-560m", # Small model; stupid but works for testing
+#        model_id="bigscience/bloom-1b7",
+#        # model_id="meta-llama/Llama-2-7b-chat-hf", # Big model; only works on TERA
+#        # model_id="google/flan-t5-small", # Small model, is not that good. Also need to change task to "text-generation", if we use this!
+#        task="text-generation",
+#        #task="text2text-generation",
+#        model_kwargs={"temperature": 0, "max_length": 723},#, "use_auth_token": True},
+#        #device=-1, # CPU
+#        device=0, # GPU
+#    )
 
 #3) M: download and run llm locally with transformers pipeline
+def setup_model_transformers():
+    model_id="bigscience/bloom-560m"
+    #model_id="bigscience/bloom-1b7"
 
-"""
-model_id="bigscience/bloom-560m"
-huggingface_hub.login(token=hugging_face_token)
+    huggingface_hub.login(token=hugging_face_token)
 
-model_config = transformers.AutoConfig.from_pretrained(
-    model_id,
-    #use_auth_token=hugging_face_token
-)
+    model_config = transformers.AutoConfig.from_pretrained(
+        model_id,
+        #use_auth_token=hugging_face_token
+    )
 
-model = transformers.AutoModelForCausalLM.from_pretrained(
-    model_id,
-    #trust_remote_code=True,
-    config=model_config,
-    #quantization_config=bnb_config,
-    device_map=device,
-    #use_auth_token=hugging_face_token
-)
+    model = transformers.AutoModelForCausalLM.from_pretrained(
+        model_id,
+        #trust_remote_code=True,
+        config=model_config,
+        #quantization_config=bnb_config,
+        device_map=device,
+        #use_auth_token=hugging_face_token
+    )
 
-#tokenizer = transformers.AutoTokenizer.from_pretrained("bigscience/bloom-560m")
-#model = transformers.AutoModelForCausalLM.from_pretrained("bigscience/bloom-560m", device_map=device,)
+    #tokenizer = transformers.AutoTokenizer.from_pretrained("bigscience/bloom-1b7", use_auth_token=hugging_face_token)
+    #model = transformers.AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b7", device_map=device, use_auth_token=hugging_face_token)
 
-# enable evaluation mode to allow model inference
-model.eval()
-print(f"Model loaded on {device}")
+    # enable evaluation mode to allow model inference
+    model.eval()
+    print(f"Model loaded on {device}")
 
-tokenizer = transformers.AutoTokenizer.from_pretrained(
-    model_id,
-    #use_auth_token=hugging_face_token
-)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(
+        model_id,
+        #use_auth_token=hugging_face_token
+    )
 
-stop_list = ['\nHuman:', '\n```\n']
-stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
-stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
+    stop_list = ['\nHuman:', '\n```\n']
+    stop_token_ids = [tokenizer(x)['input_ids'] for x in stop_list]
+    stop_token_ids = [torch.LongTensor(x).to(device) for x in stop_token_ids]
 
-# define custom stopping criteria object
-class StopOnTokens(transformers.StoppingCriteria):
-    def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
-        for stop_ids in stop_token_ids:
-            if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
-                return True
-        return False
+    # define custom stopping criteria object
+    class StopOnTokens(transformers.StoppingCriteria):
+        def __call__(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, **kwargs) -> bool:
+            for stop_ids in stop_token_ids:
+                if torch.eq(input_ids[0][-len(stop_ids):], stop_ids).all():
+                    return True
+            return False
 
-stopping_criteria = transformers.StoppingCriteriaList([StopOnTokens()])
+    stopping_criteria = transformers.StoppingCriteriaList([StopOnTokens()])
 
-generate_text = transformers.pipeline(
-    model=model, 
-    tokenizer=tokenizer,
-    return_full_text=True,  # langchain expects the full text
-    task='text-generation',
-    # we pass model parameters here too
-    stopping_criteria=stopping_criteria,  # without this model rambles during chat
-    temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
-    max_new_tokens=64,  # max number of tokens to generate in the output
-    repetition_penalty=1.1  # without this output begins repeating
-)
+    generate_text = transformers.pipeline(
+        model=model, 
+        tokenizer=tokenizer,
+        return_full_text=True,  # langchain expects the full text
+        task='text-generation',
+        # we pass model parameters here too
+        stopping_criteria=stopping_criteria,  # without this model rambles during chat
+        temperature=0.1,  # 'randomness' of outputs, 0.0 is the min and 1.0 the max
+        max_new_tokens=64,  # max number of tokens to generate in the output
+        #repetition_penalty=1.1  # without this output begins repeating -> M: for some reason, this doesn't work well with vector db!
+    )
 
-llm = HuggingFacePipeline(pipeline=generate_text)
-"""
+    #from transformers import pipeline
+    #generate_text = pipeline("text-generation", model="bigscience/bloom-560m", max_new_tokens=723, 
+    #                          device=0,)
 
+    llm = HuggingFacePipeline(pipeline=generate_text)
+    return llm
+
+llm = setup_model_transformers()
 
 #print(llm("translate to German: How are you"))
 
@@ -113,9 +122,13 @@ print("parse in text documents")
 loader = DirectoryLoader("txt_data/", glob="**/*.txt" )
 documents = loader.load()
 
+#web_links = ["https://www.databricks.com/","https://help.databricks.com","https://databricks.com/try-databricks","https://help.databricks.com/s/","https://docs.databricks.com","https://kb.databricks.com/","http://docs.databricks.com/getting-started/index.html","http://docs.databricks.com/introduction/index.html","http://docs.databricks.com/getting-started/tutorials/index.html","http://docs.databricks.com/release-notes/index.html","http://docs.databricks.com/ingestion/index.html","http://docs.databricks.com/exploratory-data-analysis/index.html","http://docs.databricks.com/data-preparation/index.html","http://docs.databricks.com/data-sharing/index.html","http://docs.databricks.com/marketplace/index.html","http://docs.databricks.com/workspace-index.html","http://docs.databricks.com/machine-learning/index.html","http://docs.databricks.com/sql/index.html","http://docs.databricks.com/delta/index.html","http://docs.databricks.com/dev-tools/index.html","http://docs.databricks.com/integrations/index.html","http://docs.databricks.com/administration-guide/index.html","http://docs.databricks.com/security/index.html","http://docs.databricks.com/data-governance/index.html","http://docs.databricks.com/lakehouse-architecture/index.html","http://docs.databricks.com/reference/api.html","http://docs.databricks.com/resources/index.html","http://docs.databricks.com/whats-coming.html","http://docs.databricks.com/archive/index.html","http://docs.databricks.com/lakehouse/index.html","http://docs.databricks.com/getting-started/quick-start.html","http://docs.databricks.com/getting-started/etl-quick-start.html","http://docs.databricks.com/getting-started/lakehouse-e2e.html","http://docs.databricks.com/getting-started/free-training.html","http://docs.databricks.com/sql/language-manual/index.html","http://docs.databricks.com/error-messages/index.html","http://www.apache.org/","https://databricks.com/privacy-policy","https://databricks.com/terms-of-use"] 
+#loader = WebBaseLoader(web_links)
+#documents = loader.load()
+
 # M: split the parsed documents into smaller chunks
 print("doing text splitting")
-text_splitter = CharacterTextSplitter(chunk_size = 100, chunk_overlap=0)
+text_splitter = RecursiveCharacterTextSplitter(chunk_size = 1000, chunk_overlap=20)
 docs = text_splitter.split_documents(documents=documents)
 #docs = text_splitter.create_documents(docs) # M: use this for string input, e.g. from file.read()
 
@@ -150,6 +163,7 @@ qna = ConversationalRetrievalChain.from_llm(
 
 #query = "what is gratuity?"
 query = "give me all phone numbers from contacts"
+#query = "What is Data lakehouse architecture in Databricks?"
 chat_history = []
 print("Query: ", query)
 result = qna({"question": query, "chat_history": chat_history})
